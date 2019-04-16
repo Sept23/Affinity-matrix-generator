0ef97bec9443a90e65e5fbf72c9b62e248663c1c
Sept23 <1127170114@qq.com>
2019-04-16 14:15:10 +0930

    Initial Commit

---
+++ assignemt1DDDM/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  assignemt1DDDM
+//
+//  Created by Liu Qinghao on 2019/4/16.
+//  Copyright © 2019 Liu Qinghao. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
